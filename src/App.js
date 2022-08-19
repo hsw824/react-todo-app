abb20fe5908b36abcc8ae9e7175f4d1faa52e95a
hsw824 <ghdtjddn0824@naver.com>
2022-08-19 17:59:34 +0900

    Feat: Create todo list structure
    (~lectuer15)

--- src/App.js
+++ src/App.js
@@ -1,7 +1,36 @@
 import "./App.css";
 
 function App() {
-  return <div></div>;
+  const todoData = [
+    {
+      id: "1",
+      title: "공부하기",
+      completed: true,
+    },
+    {
+      id: "2",
+      title: "청소하기",
+      completed: false,
+    },
+  ];
+  return (
+    <div className="container">
+      <div className="todoBlock">
+        <div className="title">
+          <h1>할 일 목록</h1>
+        </div>
+        {todoData.map((data) => {
+          return (
+            <div className="listStyle" key={data.id}>
+              <input type="checkbox" defaultChecked={data.completed} />
+              {data.title}
+              <button className="btnStyle">x</button>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
 
 export default App;
