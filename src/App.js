86c22e7fe4e37bfa567cd20ed9679dffa952209a
hsw824 <ghdtjddn0824@naver.com>
2022-08-20 22:25:23 +0900

    Feat: Create to-do list add-ons

--- src/App.js
+++ src/App.js
@@ -2,22 +2,27 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [todoData, setTodoData] = useState([
-    {
-      id: "1",
-      title: "공부하기",
-      completed: true,
-    },
-    {
-      id: "2",
-      title: "청소하기",
-      completed: false,
-    },
-  ]);
+  const [todoData, setTodoData] = useState([]);
+  const [value, setValue] = useState("");
 
   const handleClick = (id) => {
     const newData = todoData.filter((data) => data.id !== id);
     setTodoData(newData);
+  };
+
+  const onChange = (event) => {
+    setValue(event.currentTarget.value);
+  };
+
+  const handleOnSubmit = (event) => {
+    event.preventDefault();
+    const newTodo = {
+      id: Date.now(),
+      title: value,
+      completed: false,
+    };
+    setTodoData((prev) => [...prev, newTodo]);
+    setValue("");
   };
   return (
     <div className="container">
@@ -25,6 +30,21 @@ function App() {
         <div className="title">
           <h1>할 일 목록</h1>
         </div>
+        <form style={{ display: "flex" }} onSubmit={handleOnSubmit}>
+          <input
+            style={{ flex: "10", padding: "5px" }}
+            type="text"
+            value={value}
+            onChange={onChange}
+            placeholder="해야 할 일을 입력하세요"
+          />
+          <input
+            type="submit"
+            value="입력"
+            style={{ flex: "1" }}
+            className="btn"
+          />
+        </form>
         {todoData.map((data) => {
           return (
             <div className="listStyle" key={data.id}>
